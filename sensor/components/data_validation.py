b44680ae3a53fb7a08f021a376f8396628cf2823
jaysutrave <jaysutrave07@gmail.com>
2023-01-01 19:42:29 +0530

    update data validation

---
+++ sensor/components/data_validation.py
@@ -0,0 +1,176 @@
+from sensor.entity import artifact_entity,config_entity
+from sensor.exception import SensorException
+from sensor.logger import logging
+from typing import Optional
+from scipy.stats import ks_2samp
+import os,sys
+import pandas as pd
+from sensor import utils
+import numpy as np
+class data_validation:
+
+    def __init__(self
+                ,data_validation_config:config_entity.DataValidationConfig
+                ,data_ingestion_artifact:artifact_entity.DataIngestionArtifact):
+        try:
+            logging.info(f"{'>>'*20} Data Validation {'>>'*20}")
+            self.Data_validation_config = data_validation_config
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.validation_error = dict()
+        except Exception as e:
+            raise SensorException(e, sys)
+
+    
+
+    def drop_missing_columns(self,df:pd.DataFrame,report_key_name:str)->Optional[pd.DataFrame]:
+        """ this function will  drop the Columns which contain then missing  value more than sepcified threshold 
+        
+        df:Accepts a pandas datafrom
+        threshold :percentage crietra to drop a columns
+        ================================================================
+        return pandas DataFrame
+        if atleast a single columns is available after missing columns drop
+        """
+        try:
+            
+            threshold = self.Data_validation_config.missing_threshold
+            null_report =  df.isna().sum()/df.shape[0]
+            #select columns which is having high missing values i.e greater than 0.3%
+            logging.info(f"select columns which is having high missing values i.e greater than {threshold}")
+            drop_columns_name=null_report[null_report>threshold].index
+            logging.info(f"coloumns to be dropped {list(drop_columns_name)}")
+            self.validation_error[report_key_name]=list(drop_columns_name)
+            df.drop(list(drop_columns_name),axis = 1 ,inplace = True)
+            #return none no columns left
+            if len(df.columns) ==0:
+                return None
+            return df
+            
+            
+        except Exception as e:
+            raise SensorException(e, sys)
+    
+    def is_required_columns_exist(self,base_df:pd.DataFrame,current_df:pd.DataFrame,report_key_name:str)->bool:
+        try:
+            
+            base_columns = base_df.columns
+            current_columns = current_df.columns
+            missing_columns =[]
+            for base_column in base_columns:
+                if base_column not in current_columns:
+                    logging.info(f"coloumns :[ {base} is not available.")
+                    missing_columns.append(base_column)
+
+            if len(missing_columns) >0:
+                self.validation_error[report_key_name] = missing_columns
+                return False
+            return True
+        except Exception as e:
+            raise SensorException(e, sys)
+    """   
+    def data_drift(self,base_df:pd.DataFrame,current_df:pd.DataFrame,report_key_name:str):
+        try:
+            drift_report = dict()
+            base_columns = base_df.columns
+            current_column = current_df.columns
+            for base_column in base_columns:
+                base_data,current_data = base_df[base_column],current_df[base_column]
+                #Null hypothesis is that both column data drawn from same distrubtion 
+                same_distrubtion=k2_samp(base_data,current_data)
+                if same_distrubtion.pvalue>0.05:
+                    #we are accpecting the null hypothesis
+                    drift_report[base_column]={
+                        "pvalue":float(same_distribution.pvalue),
+                        "same_distrubtion":True
+                    }
+                else:
+                    #diffrent distrubtion
+                    drift_report[base_column]={
+                        "pvalue":float(same_distribution.pvalue),
+                        "same_distrubtion":False
+                    }
+                self.validation_error[report_key_name]=drift_report
+        except Exception as e:
+            raise SensorException(e, sys)
+        """
+    def data_drift(self,base_df:pd.DataFrame,current_df:pd.DataFrame,report_key_name:str):
+        try:
+            drift_report=dict()
+
+            base_columns = base_df.columns
+            current_columns = current_df.columns
+
+            for base_column in base_columns:
+                base_data,current_data = base_df[base_column],current_df[base_column]
+                #Null hypothesis is that both column data drawn from same distrubtion
+                
+                logging.info(f"Hypothesis {base_column}: {base_data.dtype}, {current_data.dtype} ")
+                same_distribution =ks_2samp(base_data,current_data)
+
+                if same_distribution.pvalue>0.05:
+                    #We are accepting null hypothesis
+                    drift_report[base_column]={
+                        "pvalues":float(same_distribution.pvalue),
+                        "same_distribution": True
+                    }
+                else:
+                    drift_report[base_column]={
+                        "pvalues":float(same_distribution.pvalue),
+                        "same_distribution":False
+                    }
+                    #different distribution
+
+            self.validation_error[report_key_name]=drift_report
+        except Exception as e:
+            raise SensorException(e, sys)
+
+    def initiate_data_validation(self)->artifact_entity.DataValidationArtifact:
+        try:
+            logging.info(f"Reading Base data frame ")
+            base_df = pd.read_csv(self.Data_validation_config.base_file_path)
+            base_df.replace({"na":np.NAN},inplace=True)
+            logging.info(f"replace na value with base df")
+            #base has na as null
+            logging.info(f"drop nulll columns from base df")
+            base_df = self.drop_missing_columns(df=base_df,report_key_name="missing_values_within_base_dataset")
+
+
+            logging.info(f"reading train dataframe")
+            train_df = pd.read_csv(self.data_ingestion_artifact.train_file_path)
+            logging.info(f"reading test dataframe")
+            test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+
+            logging.info(f"droping null values from train df") 
+
+            train_df=self.drop_missing_columns(df=train_df,report_key_name="missing_values_within_train_dataset")
+            logging.info(f"droping null values from test df") 
+            test_df=self.drop_missing_columns(df=test_df,report_key_name="missing_values_within_test_dataset")
+
+            exclude_columns = ["class"]
+            base_df = utils.convert_columns_float(df=base_df, exclude_columns=exclude_columns)
+            train_df = utils.convert_columns_float(df=train_df, exclude_columns=exclude_columns)
+            test_df = utils.convert_columns_float(df=test_df, exclude_columns=exclude_columns)
+
+            logging.info(f"is all required columns present in train df") 
+            train_df_columns_status = self.is_required_columns_exist(base_df=base_df, current_df=train_df,report_key_name="missing_column_within_train_dataset")
+            logging.info(f"is all required columns present in test df") 
+            test_df_columns_status = self.is_required_columns_exist(base_df=base_df, current_df=test_df,report_key_name="missing_column_within_test_dataset")
+
+            if train_df_columns_status:
+                logging.info(f"as all columns available train df hence detecting data drift") 
+                self.data_drift(base_df=base_df, current_df=train_df,report_key_name="data_drift_within_train_dataset")
+            if test_df_columns_status:
+                logging.info(f"as all columns available test df hence detecting data drift") 
+                self.data_drift(base_df=base_df, current_df=test_df,report_key_name="data_drift_within_test_dataset")
+
+            #write the reprot
+            logging.info(f"wiritng report in yaml file ") 
+            utils.write_ymal_file(file_path=self.Data_validation_config.report_file_path,
+                data=self.validation_error)
+                
+            data_validation_artifact=artifact_entity.DataValidationArtifact(report_file_path=self.Data_validation_config.report_file_path)
+            logging.info(f"data validation artifact {data_validation_artifact} ") 
+            return data_validation_artifact
+        except Exception as e:
+            raise SensorException(e, sys)
+        
--- sensor/utils.py
+++ sensor/utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sensor.logger import logging
 from sensor.exception import SensorException
 from sensor.config import mongo_client
+import yaml
 import os,sys
 
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
@@ -25,3 +26,21 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
         return df
     except Exception as e:
         raise SensorException(e, sys)
+def write_ymal_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise SensorException(e, sys)
+
+def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                df[column]=df[column].astype('float')
+        return df
+    except Exception as e:
+        raise e
+        
