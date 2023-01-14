28a4d146890b1c0943aafc4fdcd2cfdf90f0c3f2
jaysutrave <jaysutrave07@gmail.com>
2023-01-14 15:07:06 +0530

    completed model training

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import sys,os
 from sensor.entity import config_entity
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import data_validation
+from sensor.components.data_transformation import DataTransformation
+from sensor.components.model_trainer import ModelTrainer
+#from sensor.components.model_evaluation import ModelEvaluation
 
 
 
@@ -19,5 +22,25 @@ if __name__=="__main__":
           data_validation=data_validation(data_validation_config=data_validation_config,
                           data_ingestion_artifact=data_ingestion_artifact)
           data_validation_artifact = data_validation.initiate_data_validation()
+
+
+          #data transformation
+          data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+          data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
+          data_ingestion_artifact=data_ingestion_artifact)
+          data_transformation_artifact = data_transformation.initiate_data_transformation()
+          
+          #model trainer
+          model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+          model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+          model_trainer_artifact = model_trainer.initiate_model_trainer()
+          '''
+          model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
+          model_eval  = ModelEvaluation(model_eval_config=model_eval_config,
+           data_ingestion_artifact=data_ingestion_artifact,
+           data_transformation_artifact=data_transformation_artifact,
+            model_trainer_artifact=model_trainer_artifact)
+          model_eval_artifact = model_eval.initiate_model_evaluation()
+          '''
      except Exception as e:
           print(e)
