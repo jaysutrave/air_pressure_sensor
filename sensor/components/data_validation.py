030c42714b9e060d1f5ec874679d076570013c98
jaysutrave <jaysutrave07@gmail.com>
2023-01-14 14:16:07 +0530

    data transformation complete

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -7,6 +7,7 @@ import os,sys
 import pandas as pd
 from sensor import utils
 import numpy as np
+from sensor.config import TARGET_COLUMN
 class data_validation:
 
     def __init__(self
@@ -146,7 +147,7 @@ class data_validation:
             logging.info(f"droping null values from test df") 
             test_df=self.drop_missing_columns(df=test_df,report_key_name="missing_values_within_test_dataset")
 
-            exclude_columns = ["class"]
+            exclude_columns = [TARGET_COLUMN]
             base_df = utils.convert_columns_float(df=base_df, exclude_columns=exclude_columns)
             train_df = utils.convert_columns_float(df=train_df, exclude_columns=exclude_columns)
             test_df = utils.convert_columns_float(df=test_df, exclude_columns=exclude_columns)
